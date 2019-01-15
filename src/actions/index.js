de89a778acf4e7db35cc3ca930c108e820110e6b
Robert Lowe <robertmlowe@rmlowe.com>
2019-01-15 16:29:17 +0800

    Behind the Scenes of Redux Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get('/posts');
+  return function(dispatch, getState) {
+    const promise = jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: promise
+    return {
+      type: 'FETCH_POSTS',
+      payload: promise
+    };
   };
 };
